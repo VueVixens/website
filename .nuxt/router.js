38199519b61db82606193f0165490f9d35b13ab5
Alexandria Roberts <alexandria.roberts@joyjet.com>
2019-10-11 17:22:42 +0800

    Update Form

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -1,144 +1,83 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
 
-const _53631adf = () => interopDefault(import('../pages/about.vue' /* webpackChunkName: "pages/about" */))
-const _59761ecc = () => interopDefault(import('../pages/Credits.vue' /* webpackChunkName: "pages/Credits" */))
-const _f20dc4b8 = () => interopDefault(import('../pages/gallery.vue' /* webpackChunkName: "pages/gallery" */))
-const _3a8422e6 = () => interopDefault(import('../pages/library.vue' /* webpackChunkName: "pages/library" */))
-const _a1e94b7e = () => interopDefault(import('../pages/Request.vue' /* webpackChunkName: "pages/Request" */))
-const _7aa17937 = () => interopDefault(import('../pages/sponsors.vue' /* webpackChunkName: "pages/sponsors" */))
-const _46e85bfb = () => interopDefault(import('../pages/team.vue' /* webpackChunkName: "pages/team" */))
-const _8dc058da = () => interopDefault(import('../pages/ThankYou.vue' /* webpackChunkName: "pages/ThankYou" */))
-const _4cba9aba = () => interopDefault(import('../pages/events/_event.vue' /* webpackChunkName: "pages/events/_event" */))
-const _0d0cbc36 = () => interopDefault(import('../pages/form/_form.vue' /* webpackChunkName: "pages/form/_form" */))
-const _4a32d670 = () => interopDefault(import('../pages/galleries/_gallery.vue' /* webpackChunkName: "pages/galleries/_gallery" */))
-const _731af5a4 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _3d928798 = () => interopDefault(import('..\\pages\\about.vue' /* webpackChunkName: "pages_about" */))
+const _7e32823e = () => interopDefault(import('..\\pages\\Credits.vue' /* webpackChunkName: "pages_Credits" */))
+const _232c848e = () => interopDefault(import('..\\pages\\gallery.vue' /* webpackChunkName: "pages_gallery" */))
+const _4a2e8ea2 = () => interopDefault(import('..\\pages\\library.vue' /* webpackChunkName: "pages_library" */))
+const _167bfa56 = () => interopDefault(import('..\\pages\\Request.vue' /* webpackChunkName: "pages_Request" */))
+const _0144dbc2 = () => interopDefault(import('..\\pages\\sponsors.vue' /* webpackChunkName: "pages_sponsors" */))
+const _3fbe87f4 = () => interopDefault(import('..\\pages\\team.vue' /* webpackChunkName: "pages_team" */))
+const _3fc3361e = () => interopDefault(import('..\\pages\\ThankYou.vue' /* webpackChunkName: "pages_ThankYou" */))
+const _03682e1d = () => interopDefault(import('..\\pages\\events\\_event.vue' /* webpackChunkName: "pages_events__event" */))
+const _c8e51d74 = () => interopDefault(import('..\\pages\\form\\_form.vue' /* webpackChunkName: "pages_form__form" */))
+const _0defbf2c = () => interopDefault(import('..\\pages\\galleries\\_gallery.vue' /* webpackChunkName: "pages_galleries__gallery" */))
+const _00ee96f9 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages_index" */))
 
 Vue.use(Router)
 
-if (process.client) {
-  if ('scrollRestoration' in window.history) {
-    window.history.scrollRestoration = 'manual'
+export const routerOptions = {
+  mode: 'history',
+  base: decodeURI('/'),
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
 
-    // reset scrollRestoration to auto when leaving page, allowing page reload
-    // and back-navigation from other pages to use the browser to restore the
-    // scrolling position.
-    window.addEventListener('beforeunload', () => {
-      window.history.scrollRestoration = 'auto'
-    })
+  routes: [{
+    path: "/about",
+    component: _3d928798,
+    name: "about"
+  }, {
+    path: "/Credits",
+    component: _7e32823e,
+    name: "Credits"
+  }, {
+    path: "/gallery",
+    component: _232c848e,
+    name: "gallery"
+  }, {
+    path: "/library",
+    component: _4a2e8ea2,
+    name: "library"
+  }, {
+    path: "/Request",
+    component: _167bfa56,
+    name: "Request"
+  }, {
+    path: "/sponsors",
+    component: _0144dbc2,
+    name: "sponsors"
+  }, {
+    path: "/team",
+    component: _3fbe87f4,
+    name: "team"
+  }, {
+    path: "/ThankYou",
+    component: _3fc3361e,
+    name: "ThankYou"
+  }, {
+    path: "/events/:event?",
+    component: _03682e1d,
+    name: "events-event"
+  }, {
+    path: "/form/:form?",
+    component: _c8e51d74,
+    name: "form-form"
+  }, {
+    path: "/galleries/:gallery?",
+    component: _0defbf2c,
+    name: "galleries-gallery"
+  }, {
+    path: "/",
+    component: _00ee96f9,
+    name: "index"
+  }],
 
-    // Setting scrollRestoration to manual again when returning to this page.
-    window.addEventListener('load', () => {
-      window.history.scrollRestoration = 'manual'
-    })
-  }
-}
-const scrollBehavior = function (to, from, savedPosition) {
-  // if the returned position is falsy or an empty object,
-  // will retain current scroll position.
-  let position = false
-
-  // if no children detected and scrollToTop is not explicitly disabled
-  if (
-    to.matched.length < 2 &&
-    to.matched.every(r => r.components.default.options.scrollToTop !== false)
-  ) {
-    // scroll to the top of the page
-    position = { x: 0, y: 0 }
-  } else if (to.matched.some(r => r.components.default.options.scrollToTop)) {
-    // if one of the children has scrollToTop option set to true
-    position = { x: 0, y: 0 }
-  }
-
-  // savedPosition is only available for popstate navigations (back button)
-  if (savedPosition) {
-    position = savedPosition
-  }
-
-  return new Promise((resolve) => {
-    // wait for the out transition to complete (if necessary)
-    window.$nuxt.$once('triggerScroll', () => {
-      // coords will be used if no selector is provided,
-      // or if the selector didn't match any element.
-      if (to.hash) {
-        let hash = to.hash
-        // CSS.escape() is not supported with IE and Edge.
-        if (typeof window.CSS !== 'undefined' && typeof window.CSS.escape !== 'undefined') {
-          hash = '#' + window.CSS.escape(hash.substr(1))
-        }
-        try {
-          if (document.querySelector(hash)) {
-            // scroll to anchor by returning the selector
-            position = { selector: hash }
-          }
-        } catch (e) {
-          console.warn('Failed to save scroll position. Please add CSS.escape() polyfill (https://github.com/mathiasbynens/CSS.escape).')
-        }
-      }
-      resolve(position)
-    })
-  })
+  fallback: false
 }
 
-export function createRouter() {
-  return new Router({
-    mode: 'history',
-    base: decodeURI('/'),
-    linkActiveClass: 'nuxt-link-active',
-    linkExactActiveClass: 'nuxt-link-exact-active',
-    scrollBehavior,
-
-    routes: [{
-      path: "/about",
-      component: _53631adf,
-      name: "about"
-    }, {
-      path: "/Credits",
-      component: _59761ecc,
-      name: "Credits"
-    }, {
-      path: "/gallery",
-      component: _f20dc4b8,
-      name: "gallery"
-    }, {
-      path: "/library",
-      component: _3a8422e6,
-      name: "library"
-    }, {
-      path: "/Request",
-      component: _a1e94b7e,
-      name: "Request"
-    }, {
-      path: "/sponsors",
-      component: _7aa17937,
-      name: "sponsors"
-    }, {
-      path: "/team",
-      component: _46e85bfb,
-      name: "team"
-    }, {
-      path: "/ThankYou",
-      component: _8dc058da,
-      name: "ThankYou"
-    }, {
-      path: "/events/:event?",
-      component: _4cba9aba,
-      name: "events-event"
-    }, {
-      path: "/form/:form?",
-      component: _0d0cbc36,
-      name: "form-form"
-    }, {
-      path: "/galleries/:gallery?",
-      component: _4a32d670,
-      name: "galleries-gallery"
-    }, {
-      path: "/",
-      component: _731af5a4,
-      name: "index"
-    }],
-
-    fallback: false
-  })
+export function createRouter () {
+  return new Router(routerOptions)
 }
---
+++ assets/translations/form.js
@@ -0,0 +1,53 @@
+export default {
+  en: {
+    genderQuestion: {
+      heading: `Vue Vixens Workshops are for women and people who identify as such.
+            Before we start, please tell us whether you identify as a woman.`,
+      yes: "Yes",
+      no: "No"
+    },
+    heading: "Even Registration Form",
+    email: "Email *",
+    emailRules: "Email is required *",
+    name: "Full Name *",
+    nameRules: "Name is required *",
+    affiliation: "Affiliation",
+    foodPreferences: "Food Preferences (if any)",
+    gdpr: "I agree to receive emails from VueVixens *",
+    gdprRules: "Please check this checkbox to finish registration",
+    content: {
+      paragraph1: `You can unsubscribe at any time by clicking the link in the footer of our emails.`,
+      paragraph2: `We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your information will be transferred to Mailchimp for processing.`,
+      question: `Learn more about Mailchimp's privacy practices here.`
+    },
+    submit: "Submit",
+    raised: "Sorry, we cannot register you for this workshop.",
+    backToHome: "Back to home"
+  },
+  es: {
+    genderQuestion: {
+      heading: `Los talleres Vue Vixens son para mujeres y personas que se identifican como tales.
+            Antes de comenzar, díganos si se identifica como mujer.`,
+      yes: "Si",
+      no: "¡No!"
+    },
+    heading: "Formulario de registro par",
+    email: "Email *",
+    emailRules: "Correo electronico es requerido *",
+    name: "Nombre Completo *",
+    nameRules: "Se Requiere El nombre *",
+    affiliation: "Afiliación",
+    foodPreferences: "Preferencias de alimentos (si hay)",
+    gdpr: "Acepto recibir correos electrónicos de VueVixens*",
+    gdprRules: "Marque esta casilla de verificación para finalizar el registro",
+    content: {
+      paragraph1: `Puede darse de baja en cualquier momento haciendo clic en el enlace en el pie de página de nuestros correos electrónicos.`,
+      paragraph2: `Usamos Mailchimp como nuestra plataforma de marketing. Al hacer clic a continuación para suscribirse, usted reconoce que su información será transferida a Mailchimp para su procesamiento.`,
+      question: `Obtenga más información sobre las prácticas de privacidad de Mailchimp aquí.`
+    },
+    submit: "Enviar",
+    raised: "Lo sentimos, no podemos inscribirte en este taller.",
+    backToHome: "De vuelta a casa"
+  },
+  jp: {}
+};
