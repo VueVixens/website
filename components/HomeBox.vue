05b7131f2417720a17cd18357e0e2ea2acf21c48
Lynne Finnigan <lynnewritescode@gmail.com>
2020-10-09 13:46:35 +0100

    Update v-card on homepage with improve spacing & styling

--- components/HomeBox.vue
+++ components/HomeBox.vue
@@ -1,15 +1,18 @@
 <template>
   <v-flex class="vv-cta">
     <v-container>
-      <v-layout class="card" align-center row wrap justify-space-between>
-        <v-flex xs12 sm12 md6 class="text-center" order-md1>
+      <v-layout class="card" align-center wrap justify-space-between>
+        <v-flex xs12 sm5 md6 class="text-center" order-md1>
           <v-img class="mentor" src="/images/mentoring.jpg"></v-img>
         </v-flex>
-        <v-flex xs12 sm12 md6 order-md2>
-          <h2 class="vv-subheading font-lato text-center text-md-right">Workshops and Events</h2>
-          <h3 class="vv-sub-subheading font-lato text-center text-md-right">Women helping women</h3>
-          <v-flex text-center text-md-right>
-            <v-btn class="cta-react" color="secondary darken-1">React Workshops coming soon</v-btn>
+        <v-flex xs12 sm7 md6 order-md2 vv-card-text text-sm-left text-md-center>
+          <h2 class="vv-subheading font-lato">Workshops and Events</h2>
+          <p class="font-lato">Women helping women</p>
+          <v-flex>
+            <v-btn class="cta-react disabled" color="secondary darken-1"
+              >React Workshops<br />
+              coming soon</v-btn
+            >
           </v-flex>
         </v-flex>
       </v-layout>
@@ -19,37 +22,58 @@
 
 <style scoped lang="scss">
 .card {
-  border: 1px solid #ccc;
-  border-radius: 5px;
+  border-radius: 0;
+  -moz-box-shadow: 0 0 5px #d4d4d4;
+  -webkit-box-shadow: 0 0 5px#d4d4d4;
+  box-shadow: 0 0 5px #d4d4d4;
   padding-right: 10px;
-}
-.cta-react {
-  margin-top: 30px;
 }
 
 .vv-card-text {
-  padding-top: 15px;
+  padding: 20px 15px;
 }
 
-.mentor {
-  max-width: 800px;
+.cta-react {
+  width: 100%;
+  padding: 10px !important;
+  height: auto !important;
+  text-align: center;
 }
 
-@media (max-width: 960px) {
+@media (min-width: 421px) {
   .cta-react {
+    width: auto;
     padding-top: 10px;
     text-align: center;
+
+    br {
+      display: none;
+    }
   }
+
   .card {
-    padding: 0px 0px 10px 0px;
-    margin: 5px;
+    padding: 0px;
+    margin: 5px 5px 30px;
   }
 }
 
-@media (max-width: 420px) {
+@media (min-width: 600px) {
+  .vv-card-text {
+    padding: 20px 25px;
+  }
+
   .cta-react {
-    padding-top: 10px;
-    text-align: center;
+    font-size: 14px;
+  }
+}
+
+@media (min-width: 960px) {
+  .vv-card-text {
+    padding: 20px 30px;
+  }
+
+  .cta-react {
+    font-size: 16px;
   }
 }
 </style>
