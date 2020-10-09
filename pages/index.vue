256b18f7cb60aea0a7bed7208f891f84058645b8
Lynne Finnigan <lynnewritescode@gmail.com>
2020-10-09 13:57:40 +0100

    Fix hero buttons at 960 breakpoint & change media queries to min-width

--- pages/index.vue
+++ pages/index.vue
@@ -4,30 +4,38 @@
       <v-container fill-height>
         <v-layout align-center row wrap justify-space-between>
           <v-flex xs12 sm12 md4 class="text-center" order-md2>
-            <img class="vv-logo" src="~/assets/images/ff-logo.png" alt="Front-End Foxes logo" />
+            <img
+              class="vv-logo"
+              src="~/assets/images/ff-logo.png"
+              alt="Front-End Foxes logo"
+            />
           </v-flex>
           <v-flex xs12 sm12 md7 order-md1>
-            <h1 class="vv-heading font-lato text-center text-md-left">{{$t('heading.title')}}</h1>
-            <h2
-              class="vv-subheading font-lato text-center text-md-left"
-            >{{$t('heading.description')}}</h2>
+            <h1 class="vv-heading font-lato text-center text-md-left">
+              {{ $t("heading.title") }}
+            </h1>
+            <h2 class="vv-subheading font-lato text-center text-md-left">
+              {{ $t("heading.description") }}
+            </h2>
             <v-flex class="vv-cta">
               <v-btn
                 href="https://workshops.vuevixens.org/workshops/vue/"
                 target="_blank"
                 color="accent darken-1"
-              >Vue Workshops</v-btn>
+                >Vue Workshops</v-btn
+              >
               <v-btn
                 href="https://www.meetup.com/frontendfoxes"
                 target="_blank"
                 color="accent darken-1"
-              >Meetups</v-btn>
+                >Meetups</v-btn
+              >
               <v-btn
-              
                 href="https://www.patreon.com/vuevixens"
                 target="_blank"
                 color="accent darken-1"
-              >Patreon</v-btn>
+                >Patreon</v-btn
+              >
             </v-flex>
           </v-flex>
         </v-layout>
@@ -35,7 +43,10 @@
     </v-parallax>
     <div class="vv-homebox">
       <VVHomeBox />
-      <VVAnnouncements v-if="announcements && announcements.length" :announcements="announcements" />
+      <VVAnnouncements
+        v-if="announcements && announcements.length"
+        :announcements="announcements"
+      />
     </div>
   </div>
 </template>
@@ -50,39 +61,37 @@ export default {
   mixins: [storyblok],
   components: {
     VVAnnouncements,
-    VVHomeBox
+    VVHomeBox,
   },
 
   computed: {
     announcements() {
       return this.story.content.body;
-    }
+    },
   },
   i18n: {
-    messages
+    messages,
   },
-  transition: "fade"
+  transition: "fade",
 };
 </script>
 
 <style scoped lang="scss">
-.vv-logo {
-  max-width: 100%;
+.vv-heading {
+  padding-bottom: 10px;
+  line-height: 100%;
 }
 
-.vv-heading {
-  padding-bottom: 30px;
+.vv-logo {
+  max-width: 160px;
 }
 
 .vv-cta {
-  padding-top: 30px;
+  padding-top: 10px;
+  text-align: center;
 }
 
-.vv-card-text {
-  padding-top: 15px;
-}
-
-@media (max-width: 960px) {
+@media (min-width: 420px) {
   .vv-heading {
     padding-bottom: 15px;
   }
@@ -92,27 +101,24 @@ export default {
   }
 
   .vv-cta {
-    padding-top: 10px;
-    text-align: center;
-  }
-  .vv-cta .v-btn {
-    margin-top: 10px;
+    .v-btn {
+      margin-top: 10px;
+    }
   }
 }
 
-@media (max-width: 420px) {
+@media (min-width: 960px) {
   .vv-heading {
-    padding-bottom: 10px;
-    line-height: 100%;
+    padding-bottom: 30px;
   }
 
   .vv-logo {
-    max-width: 160px;
+    max-width: 100%;
   }
 
   .vv-cta {
-    padding-top: 10px;
-    text-align: center;
+    text-align: left;
+    padding-top: 30px;
   }
 }
 </style>
